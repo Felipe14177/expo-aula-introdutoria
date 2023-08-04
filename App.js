3a4875e9b9ea4034f751b632c0e20a21e02142a6
Felipe14177 <141411365+Felipe14177@users.noreply.github.com>
2023-08-04 11:36:39 +0000

    felpz

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <View><Text>Não carregou a fonte</Text></View>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Felipe!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontFamily: 'Inter-Black',
+    fontSize: 30
+  }
+});
